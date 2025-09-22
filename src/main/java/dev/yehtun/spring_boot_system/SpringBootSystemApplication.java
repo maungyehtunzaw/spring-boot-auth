7c3676d7db177014376aa55498dec93a83232695
Z'isalway-zzz <superman@gmail.com>
2025-09-22 18:00:49 +0700

    inital project setup

---
+++ src/main/java/dev/yehtun/spring_boot_system/SpringBootSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.yehtun.spring_boot_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yehtun/spring_boot_system/SpringBootSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yehtun.spring_boot_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
